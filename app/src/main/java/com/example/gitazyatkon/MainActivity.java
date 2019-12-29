0acc7eda9a804f5709c472250ec5cd9497742291
hadid <hadid.khodadadi1995@gmail.com>
2019-12-29 21:55:01 +0330

    Initial commit

---
+++ app/src/main/java/com/example/gitazyatkon/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.gitazyatkon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Dalam", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
