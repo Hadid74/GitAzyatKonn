76e0713c7c336070475ebe2e4be4681576b288c1
hadid <hadid.khodadadi1995@gmail.com>
2019-12-29 22:04:10 +0330

    first class commit

---
+++ app/src/main/java/com/example/gitazyatkon/Test.java
@@ -0,0 +1,23 @@
+package com.example.gitazyatkon;
+
+public class Test {
+    String Dalam;
+    private String name;
+    private String family;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+}
